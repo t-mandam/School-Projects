67bbfd5b057cdaf0f51329775cd8f96865dc49a5
t-mandam <t_mandam@live.concordia.ca>
2025-05-29 16:11:31 -0400

    Rename

--- SolarRoofEnergyCalculator.java
+++ SolarRoofEnergyCalculator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class A1_Q1 {
+public class SolarRoofEnergyCalculator {
 
 	public static void main(String[] args) {
 		// ------------------------------------------------------
